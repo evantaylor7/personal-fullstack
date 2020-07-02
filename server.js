991ca8d510f70cfdbdf3e925665297e77e7c866c
Evan Taylor <evantaylor667@gmail.com>
2020-07-02 12:56:47 -0600

    reverted changes

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use((err, req, res, next) => {
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html', 'uploads'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
 app.listen(port, () => {
