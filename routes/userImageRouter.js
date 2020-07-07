4f5811a251c6eb3b7b09ee12f109588705e6bb41
Evan Taylor <evantaylor667@gmail.com>
2020-07-06 19:32:24 -0600

    fixed error

--- routes/userImageRouter.js
+++ routes/userImageRouter.js
@@ -9,7 +9,7 @@ const multerS3 = require('multer-s3')
 // const fs = require('fs')
 
 aws.config.region = 'us-east-1'
-aws.config.credentials = {accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_ACCESS_KEY}
+aws.config.credentials = {accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY}
 const s3 = new aws.S3()
 const bucket = process.env.S3_BUCKET_NAME
 
