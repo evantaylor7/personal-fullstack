4f1df309eade27fc030aaaf205664f871c3be9f4
Evan Taylor <evantaylor667@gmail.com>
2020-07-10 13:11:47 -0600

    added color input to titles

--- client/src/components/private/titles/ColorChooser.js
+++ client/src/components/private/titles/ColorChooser.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 
 const ColorChooser = props => {
     const {title, subtitle, description, name, update, inputs} = props
+
+    const [show, setShow] = useState(false)
 
     const handleColorChange = color => {
         const updates = {
@@ -15,23 +17,50 @@ const ColorChooser = props => {
         update(updates)
     }
 
-    const defaultColors = ['whitesmoke', '#1d1d1d', '#343837', '#214761', '#3C0008', '#0F9B8E', '#C85A53','#CB7723', '#048243', '#D0FEFE', '#8F1402']
+    const handleColorInput = e => {
+        const {value} = e.target
+        const updates = {
+            ...inputs,
+            [name]: {
+                content: inputs[name].content,
+                color: value
+            }
+        }
+        update(updates)
+    }
+
+    const defaultColors = ['whitesmoke', '#1d1d1d', '#343837', '#D0FEFE', '#0F9B8E', '#C85A53', '#CB7723']
 
     return (
-        <ColorsContainer title={title} subtitle={subtitle} description={description}>
+        <ColorsContainer title={title} subtitle={subtitle} description={description} show={show}>
             {defaultColors.map(color => 
                 <Colors key={color} onClick={() => handleColorChange(color)} color={color}/>
             )}
+            <ColorInput 
+                type='color' 
+                value={inputs[name].color} 
+                onChange={handleColorInput} 
+                onClick={() => setShow(true)}
+                onBlur={() => setShow(false)}
+            />
         </ColorsContainer>
     )
 }
 
+const ColorInput = styled.input`
+    margin-left: 1px;
+    
+    &:active {
+        display: flex;
+    }
+`
+
 const ColorsContainer = styled.div`
-    display: none;
+    display: ${props => props.show ? 'flex' : 'none'};
     flex-direction: row;
     position: absolute;
     left: 50%;
-    margin-left: -122px;
+    margin-left: -100px;
     margin-top: -18px;
 
     ${props => props.title}:hover & {
@@ -42,6 +71,9 @@ const ColorsContainer = styled.div`
     }
     ${props => props.description}:hover & {
         display: flex
+    }
+    ${ColorInput}:hover & {
+        display: flex;
     }
     @media (max-width: 750px){
         display: flex;
